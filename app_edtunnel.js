0d114f44faf6b60b6ca2fe7b832e26b771279771
yusky12 <140600437+yusky12@users.noreply.github.com>
2023-09-02 04:54:28 +0800

    Update app_edtunnel.js

--- app_edtunnel.js
+++ app_edtunnel.js
@@ -4,7 +4,7 @@ const net = require('net');
 
 // 创建 WebSocket 服务器
 const wss = new WebSocket.Server({ port: 443 });
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = 'b3ec3d0b-8e74-48ea-b583-85e6d577c790';
 let proxyIP = "64.68.192." + Math.floor(Math.random() * 255);
 
 let address = '';
